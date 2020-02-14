0b876027e1ff954fe3b582f68fb1f90ac1c7dbc6
nikola <nikola4work@gmail.com>
2020-02-14 12:46:48 -0500

    install required dependecies

---
+++ EmployeeTracker.js
@@ -0,0 +1,6 @@
+var mysql = require("mysql");
+var inquirer = req("inquirer");
+var consoleTable = require("console.table");
+var chalk = require("chalk");
+var tracker = require("./Assets/tracker");
+var queries = require("./Assets/query");
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Employee_Management_System
 # Empolyee_Tracker
