f175f5873e84d5995d77b3796a3576118dd117b0
nikola <nikola4work@gmail.com>
2020-02-14 13:04:06 -0500

    fixed error with sql,value can be transfered to js code

--- EmpInfoSchema.sql
+++ EmpInfoSchema.sql
@@ -1,29 +1,36 @@
-USE emptracker_db;
+USE employeeTracker_db;
 
 -- Insert into department table
-INSERT INTO emptracker_db.department (name)
-VALUES ("Technology"), ("Human Resources"), ("Sales/Marketing"), ("Accounting/Finance");
+INSERT INTO employeeTracker_db.department
+    (name)
+VALUES
+    ("Technology"),
+    ("Human Resources"),
+    ("Sales/Marketing"),
+    ("Accounting/Finance");
 
 -- Insert into role table
-INSERT INTO emptracker_db.role (title, salary, department_id)
+INSERT INTO employeeTracker_db.role
+    (title, salary, department_id)
 VALUES
-('Engineer', 90000, 1),
-('HR Business Partner', 80000, 2),
-('Sales Manager', 64990, 3),
-('Accountant', 75000, 4);
+    ('Engineer', 90000, 1),
+    ('HR Business Partner', 80000, 2),
+    ('Sales Manager', 64990, 3),
+    ('Accountant', 75000, 4);
 
 
-INSERT INTO emptracker_db.employee (first_name, last_name, role_id)
+INSERT INTO employeeTracker_db.employee
+    (first_name, last_name, role_id)
 VALUES
-('Gene', 'Edwards', 1),
-('Siavash', 'Ashkiani', 2),
-('Lusio', 'Filiba', 4),
-('Mravin', 'Castro', 1),
-('Niko', 'Jovanovic', 3),
-('Lawrence', 'Kirk', 1),
-('Ranjan', 'Biswas', 4);
+    ('Gene', 'Edwards', 1),
+    ('Siavash', 'Ashkiani', 2),
+    ('Lusio', 'Filiba', 4),
+    ('Mravin', 'Castro', 1),
+    ('Niko', 'Jovanovic', 3),
+    ('Lawrence', 'Kirk', 1),
+    ('Ranjan', 'Biswas', 4);
 
 -- Add a manager to two employee
-UPDATE emptracker_db.employee
+UPDATE employeeTracker_db.employee
 SET manager_id = 7
 WHERE (id = 1 OR id = 6)
--- EmployeeTracker.js
+++ EmployeeTracker.js
@@ -1,6 +1,21 @@
+// required dependencies
 var mysql = require("mysql");
 var inquirer = req("inquirer");
 var consoleTable = require("console.table");
 var chalk = require("chalk");
 var tracker = require("./Assets/tracker");
 var queries = require("./Assets/query");
+
+// Created connection info for the mysql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "nikola4mysql2020",
+    database: "employeeTracker_DB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    startTracker();
+});
